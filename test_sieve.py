93f9e7efaed5c3148eae9b76f157922321ea6622
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-09 11:28:22 +0530

    Adds Tests to test Tree data structure.

---
+++ test_sieve.py
@@ -0,0 +1,19 @@
+__author__ = 'saimanoj'
+
+import unittest
+import sieve
+
+
+class SieveTests(unittest.TestCase):
+    def test_1_is_not_prime(self):
+        self.assertNotIn(1, list(sieve.eratosthenes(-10)))
+
+    def test_2_is_prime(self):
+        self.assertEqual(list(sieve.eratosthenes(2)), [2])
+
+    def test_primes_in_100(self):
+        self.assertEqual(list(sieve.eratosthenes(100)),
+                         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+                          47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+
+
---
+++ tests.py
@@ -0,0 +1,10 @@
+__author__ = 'saimanoj'
+
+import test_sieve
+
+
+class Tests(test_sieve.SieveTests):
+    # Add more Test classes as base classes to this class for including them
+    # in all Tests.
+    pass
+
