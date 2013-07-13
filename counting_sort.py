1602c38d24bab7214aac324f2fe5c8f6dd06b592
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-13 20:27:07 +0530

    Modifies counting_sort to be more pythonic.

--- counting_sort.py
+++ counting_sort.py
@@ -6,13 +6,13 @@ def sort(unsorted, k):
     n = len(unsorted)
     result = [0] * n
 
-    for j in range(n):
-        counter[unsorted[j]] += 1
+    for x in unsorted:
+        counter[x] += 1
     for i in range(1, k + 1):
         counter[i] += counter[i - 1]
-    for j in range(n - 1, -1, -1):
-        result[counter[unsorted[j]] - 1] = unsorted[j]
-        counter[unsorted[j]] -= 1
+    for x in reversed(unsorted):
+        result[counter[x] - 1] = x
+        counter[x] -= 1
 
     return result
 
