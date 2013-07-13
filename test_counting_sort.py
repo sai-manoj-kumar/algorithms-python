f8b4e22363048a1cb1c3c0934ac0429df5e091c2
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-13 19:21:23 +0530

    Refactors counting sort tests to reduce redundancy.

--- test_counting_sort.py
+++ test_counting_sort.py
@@ -5,28 +5,28 @@ import counting_sort
 
 
 class CountingSortTests(unittest.TestCase):
+    def counting_sort_check(self, unsorted, expected, k):
+        actual = counting_sort.sort(unsorted, k)
+        # sorted is a built-in function, so using actual.
+        self.assertEqual(actual, expected)
+
     def test_1_repetitions_counting_sort(self):
         unsorted = [4, 1, 3, 4, 3]
         expected = [1, 3, 3, 4, 4]
-        sorted_list = counting_sort.sort(unsorted, 4)
-        # sorted is a built-in function, so sorted_list.
-        self.assertEqual(sorted_list, expected)
+        self.counting_sort_check(unsorted, expected, 4)
 
     def test_2_unique_counting_sort(self):
         unsorted = [4, 1, 7, 2, 8, 6, 5, 3]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = counting_sort.sort(unsorted, 8)
-        self.assertEqual(sorted_list, expected)
+        self.counting_sort_check(unsorted, expected, 8)
 
     def test_3_already_sorted_counting_sort(self):
         unsorted = [1, 2, 3, 4, 5, 6, 7, 8]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = counting_sort.sort(unsorted, 8)
-        self.assertEqual(sorted_list, expected)
+        self.counting_sort_check(unsorted, expected, 8)
 
     def test_3_reverse_sorted_counting_sort(self):
         unsorted = [8, 7, 6, 5, 4, 3, 2, 1]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = counting_sort.sort(unsorted, 8)
-        self.assertEqual(sorted_list, expected)
+        self.counting_sort_check(unsorted, expected, 8)
 
