af1ec82a4e9136bae0fe0fa680fdba2278fc63d7
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-13 21:21:55 +0530

    Refactors insertion sort tests to reduce redundancy

--- test_insertion_sort.py
+++ test_insertion_sort.py
@@ -5,28 +5,28 @@ import insertion_sort
 
 
 class InsertionSortTests(unittest.TestCase):
+    def insertion_sort_check(self, unsorted, expected):
+        actual = insertion_sort.sort(unsorted)
+        # sorted is a built-in function, so using actual.
+        self.assertEqual(actual, expected)
+
     def test_1_repetitions_insertion_sort(self):
         unsorted = [4, 1, 3, 4, 3]
         expected = [1, 3, 3, 4, 4]
-        sorted_list = insertion_sort.sort(unsorted)
-        # sorted is a built-in function, so sorted_list.
-        self.assertEqual(sorted_list, expected)
+        self.insertion_sort_check(unsorted, expected)
 
     def test_2_unique_insertion_sort(self):
         unsorted = [4, 1, 7, 2, 8, 6, 5, 3]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = insertion_sort.sort(unsorted)
-        self.assertEqual(sorted_list, expected)
+        self.insertion_sort_check(unsorted, expected)
 
     def test_3_already_sorted_insertion_sort(self):
         unsorted = [1, 2, 3, 4, 5, 6, 7, 8]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = insertion_sort.sort(unsorted)
-        self.assertEqual(sorted_list, expected)
+        self.insertion_sort_check(unsorted, expected)
 
     def test_3_reverse_sorted_insertion_sort(self):
         unsorted = [8, 7, 6, 5, 4, 3, 2, 1]
         expected = [1, 2, 3, 4, 5, 6, 7, 8]
-        sorted_list = insertion_sort.sort(unsorted)
-        self.assertEqual(sorted_list, expected)
+        self.insertion_sort_check(unsorted, expected)
 
