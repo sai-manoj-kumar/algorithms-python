e121492d7543adf99c3dbbd2ac58db30b1e28340
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-07-13 18:51:09 +0530

    Removes usage of ds module(data structures)

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 __author__ = 'saimanoj'
 
 import sieve
-import ds.tree
-
 
 def main():
-    t = ds.tree.Tree('/')
-    node_a = ds.tree.TreeNode('a')
-    t.add(t.root, node_a)
-    node_b = ds.tree.TreeNode('b')
-    t.add(t.root, node_b)
-    node_c = ds.tree.TreeNode('c')
-    t.add(t.root, node_c)
-    node_d = ds.tree.TreeNode('d')
-    t.add(node_c, node_d)
-    t.root.data = '//'
-    # t.remove(node_d)
-    t.get_all_nodes()
-    print 'bye'
+    print 'Hello, Algorithms World!'
 
 
 def sundaram_sieve_demo():
